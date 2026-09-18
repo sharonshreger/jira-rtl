287281e23afabfeb8703a583e5ca536c207215c1
sharonshreger <78081841+sharonshreger@users.noreply.github.com>
2026-09-18 18:42:15 +0300

    Add upstream src/content/enums/ElementType.js

---
+++ src/content/enums/ElementType.js
@@ -0,0 +1,14 @@
+/**
+ * Enum representing behavior type for matching elements
+ * 
+ * @readonly
+ * @enum {string}
+ */
+export const ElementType = {
+	/** For general text elements */
+	TEXT: 'TEXT',
+	/** For input and textarea elements */
+	INPUT: 'INPUT',
+	/** For elements with content editor */
+	CONTENT_EDITABLE: 'CONTENT_EDITABLE',
+};
